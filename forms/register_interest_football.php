7b3a0da8acbd36e47bbb7d79a6305180239b0454
conel <nkowald@conel.ac.uk>
2012-03-26 18:13:14 +0100

    Moved forms from webroot into forms dir

--- forms/register_interest_football.php
+++ forms/register_interest_football.php
@@ -2,22 +2,21 @@
 	// While developing, show errors
 	//ini_set('display_errors',1);
 	//error_reporting(E_ALL);
+
 	
 	// First make sure this file hasn't been directly accessed and only run if coming from open day page
-	if (isset($_POST['firstname']) && (isset($_SERVER['HTTP_REFERER']) && strpos($_SERVER['HTTP_REFERER'],'increase_your_employability')) ) {
-	
+	if (isset($_POST['name']) && (isset($_SERVER['HTTP_REFERER']) && strpos($_SERVER['HTTP_REFERER'],'football_academy')) ) {
+		
 		// This variable holds whether database input is successful
 		$added = FALSE;
 		
 		// This variable sets whether the SQL functions are in debug mode or not - 
 		$debug = 0; // 0 = Don't Debug, 1 = Debug
 		
-		$send_email = TRUE;
-		
 		// Required includes
-		include_once('matrix_engine/config.php');
-		include_once('matrix_engine/'.CLASSES_DIR.'db_mysql.php');
-		include_once('matrix_engine/'.CLASSES_DIR.'class_mailer.php');
+		include_once('../matrix_engine/config.php');
+		include_once('../matrix_engine/'.CLASSES_DIR.'db_mysql.php');
+		include_once('../matrix_engine/'.CLASSES_DIR.'class_mailer.php');
 		
 		/* This script inserts submitted 'Register your interest now' form data and into a database (tbl_open_day) */
 		
@@ -29,21 +28,20 @@
 		$sql->connect();
 
 		// Set up POST variables to insert, escaping them for security
-		$firstname = mysql_real_escape_string($_POST['firstname'], $sql->Link_ID);
-		$surname = mysql_real_escape_string($_POST['surname'], $sql->Link_ID);
+		$name = mysql_real_escape_string($_POST['name'], $sql->Link_ID);
 		$address_line_1 = mysql_real_escape_string($_POST['address_line_1'], $sql->Link_ID);
 		$address_line_2 = mysql_real_escape_string($_POST['address_line_2'], $sql->Link_ID);
 		$address_line_3 = mysql_real_escape_string($_POST['address_line_3'], $sql->Link_ID);
 		$postcode = mysql_real_escape_string($_POST['postcode'], $sql->Link_ID);
-		$age = mysql_real_escape_string($_POST['age'], $sql->Link_ID);
 		$telephone_landline = mysql_real_escape_string($_POST['telephone_landline'], $sql->Link_ID);
 		$telephone_mobile = mysql_real_escape_string($_POST['telephone_mobile'], $sql->Link_ID);
 		$email_address = mysql_real_escape_string($_POST['email_address'], $sql->Link_ID);
+		$age_at_31_august = mysql_real_escape_string($_POST['age_at_31_august'], $sql->Link_ID);
 
 
 		// Build INSERT query
-		$query = "INSERT INTO tbl_princes_trust (firstname, surname, address_line_1, address_line_2, address_line_3, postcode, age, telephone_landline, telephone_mobile, email_address, datetime_submitted) 
-		VALUES('$firstname', '$surname', '$address_line_1','$address_line_2','$address_line_3','$postcode', '$age', '$telephone_landline', '$telephone_mobile','$email_address','$date_now_mysql')";
+		$query = "INSERT INTO tbl_football_academy (name, address_line_1, address_line_2, address_line_3, postcode, telephone_landline, telephone_mobile, email_address, age_at_31_august, datetime_submitted) 
+		VALUES('$name', '$address_line_1','$address_line_2','$address_line_3','$postcode','$telephone_landline', '$telephone_mobile','$email_address','$age_at_31_august','$date_now_mysql')";
 		$sql->query($query,$debug);
 
 		if ($sql->num_rows_affected() > 0) {
@@ -54,12 +52,12 @@
 		
 		if ($added) {
 			
+			/*
 			if ($send_email) {
 			
 				$date_now = date('d/m/Y, H:i:s');
-				
-				/* Create email */
-				$body_html = '<p>You have received a new \'Prince\'s Trust Team Programme\' enquiry.<br /><br />Submitted: '.$date_now.'</p>';
+
+				$body_html = '<p>You have received a new \'Open Day - Register Your Interest\' form submission.<br /><br />Submitted: '.$date_now.'</p>';
 				$body_html .= '<table border="1" cellpadding="2" cellspacing="0">';
 				
 				foreach($_POST as $post_var => $value) {
@@ -75,9 +73,9 @@
 				$mail->IsHTML(TRUE); // send HTML email
 				$mail->IsSMTP(); // use SMTP to send
 				// Set Recipient
-				$mail->AddAddress('CourseInfo@staff.conel.ac.uk','Course Info');
+				$mail->AddAddress('nkowald@conel.ac.uk', 'Nathan Kowald');
 				//$mail->AddBCC('nkowald@staff.conel.ac.uk','Nathan Kowald');
-				$mail->Subject = "Prince's Trust Team Programme";
+				$mail->Subject = "Football Academy - Register your interest form submission";
 				$mail->From = 'webmaster@staff.conel.ac.uk';
 				$mail->FromName = 'Conel Website Notifications';
 				$mail->Body = $body_html;
@@ -86,20 +84,18 @@
 				$result = $mail->Send(); // send email notification!
 			
 			}
+			*/
 			
 			/*
 			if ($result) {
-				echo '<p>Email successfully sent!</p>';
-				//header('Location: http://www.conel.ac.uk/email_successfully_sent');
+				header('Location: http://www.conel.ac.uk/email_successfully_sent');
 				exit;
 			} else {
 				echo '<p>Email failed</p>';
-				//header('Location: http://www.conel.ac.uk/news_events/event_calendar/november_2009/7_november_2009_open_day?email=failed');
+				header('Location: http://www.conel.ac.uk/news_events/event_calendar/november_2009/7_november_2009_open_day?email=failed');
 				exit;
 			}
 			*/
-			
-			
 
 			header('Location: http://www.conel.ac.uk/registration_successful');
 			exit;
@@ -107,7 +103,7 @@
 			
 		} else {
 			echo '<p>Failed to add info to database</p>';
-			header('Location: http://www.conel.ac.uk/news_events/news/2010/april_2010/increase_your_employability_with_the_prince_s_trust_team_programme?email=failed');
+			header('Location: http://www.conel.ac.uk/for_learners/football_academy?email=failed');
 			exit;
 		}
 		
--- forms/register_interest_taster.php
+++ forms/register_interest_taster.php
@@ -1,12 +1,10 @@
 <?php
-	session_start();
-	
 	// While developing, show errors
 	//ini_set('display_errors',1);
 	//error_reporting(E_ALL);
 
 	// First make sure this file hasn't been directly accessed and only run if coming from open day page
-	if (isset($_POST['name']) && (isset($_SERVER['HTTP_REFERER']) && strpos($_SERVER['HTTP_REFERER'],'staff_bbq')) ) {
+	if (isset($_POST['name']) && (isset($_SERVER['HTTP_REFERER']) && strpos($_SERVER['HTTP_REFERER'],'taster_days')) ) {
 	
 		// This variable holds whether database input is successful
 		$added = FALSE;
@@ -15,9 +13,9 @@
 		$debug = 0; // 0 = Don't Debug, 1 = Debug
 		
 		// Required includes
-		include_once('matrix_engine/config.php');
-		include_once('matrix_engine/'.CLASSES_DIR.'db_mysql.php');
-		include_once('matrix_engine/'.CLASSES_DIR.'class_mailer.php');
+		include_once('../matrix_engine/config.php');
+		include_once('../matrix_engine/'.CLASSES_DIR.'db_mysql.php');
+		include_once('../matrix_engine/'.CLASSES_DIR.'class_mailer.php');
 		
 		/* This script inserts submitted 'Register your interest now' form data and into a database (tbl_open_day) */
 		
@@ -28,23 +26,24 @@
 		$sql = new DB_Sql();
 		$sql->connect();
 		
-		// set up session data to show on receipt page
-		$_SESSION['staff_bbq']['attending'] = $_POST['attending'];
-		$_SESSION['staff_bbq']['require_transport'] = $_POST['require_transport'];
-		$_SESSION['staff_bbq']['name'] = $_POST['name'];
-		$_SESSION['staff_bbq']['email'] = $_POST['email'];
-		$_SESSION['staff_bbq']['extension'] = $_POST['extension'];
-		
 		// Set up POST variables to insert, escaping them for security
-		$attending = mysql_real_escape_string($_POST['attending'], $sql->Link_ID);
-		$require_transport = mysql_real_escape_string($_POST['require_transport'], $sql->Link_ID);
+		$interested_in_csv = (is_array($_POST['interested_in'])) ? implode(', ',$_POST['interested_in']) : $_POST['interested_in'];
+		$interested_in = mysql_real_escape_string($interested_in_csv, $sql->Link_ID);
 		$name = mysql_real_escape_string($_POST['name'], $sql->Link_ID);
 		$email = mysql_real_escape_string($_POST['email'], $sql->Link_ID);
-		$extension = mysql_real_escape_string($_POST['extension'], $sql->Link_ID);
+		$telephone = mysql_real_escape_string($_POST['telephone'], $sql->Link_ID);
+		$address = mysql_real_escape_string($_POST['address'], $sql->Link_ID);
+		$postcode = mysql_real_escape_string($_POST['postcode'], $sql->Link_ID);
+		$what_school_do_you_attend = mysql_real_escape_string($_POST['what_school_do_you_attend'], $sql->Link_ID);
+		$which_borough = mysql_real_escape_string($_POST['which_borough_do_you_live_in'], $sql->Link_ID);
+		$how_heard = mysql_real_escape_string($_POST['how_did_you_hear_about_us'], $sql->Link_ID);
+		
+		// Strip HTML from textarea
+		$how_heard = strip_tags($how_heard);
 
 		// Build INSERT query
-		$query = "INSERT INTO tbl_staff_bbq (attending, require_transport, name, email, extension, datetime_submitted) 
-		VALUES('$attending','$require_transport','$name','$email','$extension','$date_now_mysql')";
+		$query = "INSERT INTO tbl_taster_day (interested_in,name,email,telephone,address,postcode,what_school_do_you_attend,which_borough_do_you_live_in,how_did_you_hear_about_us,datetime_submitted) 
+		VALUES('$interested_in','$name','$email','$telephone','$address','$postcode','$what_school_do_you_attend','$which_borough','$how_heard','$date_now_mysql')";
 		$sql->query($query,$debug);
 
 		if ($sql->num_rows_affected() > 0) {
@@ -83,24 +82,24 @@
 			//$mail->SMTPDebug = TRUE;
 
 			$result = $mail->Send(); // send email notification!
-			
+
 			if ($result) {
-				header('Location: http://www.conel.ac.uk/email_successfully_sent');
+				header('Location: http://conel-dev/email_successfully_sent');
 				exit;
 			} else {
 				echo '<p>Email failed</p>';
-				header('Location: http://www.conel.ac.uk/news_events/events_calendar/february_2010/open_days_2010?email=failed');
+				header('Location: http://conel-dev/news_events/events_calendar/february_2010/open_days_2010?email=failed');
 				exit;
 			}
 			*/
 
-			header('Location: http://www.conel.ac.uk/staff_bbq_registered');
+			header('Location: http://www.conel.ac.uk/registration_successful');
 			exit;
 
 			
 		} else {
 			echo '<p>Failed to add info to database</p>';
-			header('Location: http://www.conel.ac.uk/staff_bbq?registration=failed');
+			header('Location: http://www.conel.ac.uk/our_courses/taster_days?registration=failed');
 			exit;
 		}
 		
