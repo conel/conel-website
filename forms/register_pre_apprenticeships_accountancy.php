d04123f63720c46d191f501047cbb097e79e0f41
Nathan Kowald <nkowald@conel.ac.uk>
2012-05-01 09:47:31 +0100

    New webform

---
+++ forms/register_pre_apprenticeships_accountancy.php
@@ -0,0 +1,78 @@
+<?php
+	session_start();
+	
+	// While developing, show errors
+	//ini_set('display_errors',1);
+	//error_reporting(E_ALL);
+
+	// First make sure this file hasn't been directly accessed and only run if coming from open day page
+	if (isset($_POST['firstname']) && (isset($_SERVER['HTTP_REFERER']) && strpos($_SERVER['HTTP_REFERER'],'our_courses/aat_level_2_pre_apprenticeship')) ) {
+	
+		// This variable holds whether database input is successful
+		$added = FALSE;
+		
+		// This variable sets whether the SQL functions are in debug mode or not - 
+		$debug = 1; // 0 = Don't Debug, 1 = Debug
+		
+		// Required includes
+		include_once('../matrix_engine/config.php');
+		include_once('../matrix_engine/'.CLASSES_DIR.'db_mysql.php');
+		include_once('../matrix_engine/'.CLASSES_DIR.'class_mailer.php');
+		
+		/* This script inserts submitted 'Register your interest now' form data and into a database (tbl_open_day) */
+		
+		// what is the current date and time?
+		$date_now_mysql = date('Y-m-d H:i:s'); // "2009-06-21 14:34:04": MySQL timestamp format
+		
+		// We need to instantiate the SQL class
+		$sql = new DB_Sql();
+		$sql->connect();
+		
+		// Set up POST variables to insert, escaping them for security
+		$firstname = mysql_real_escape_string($_POST['firstname'], $sql->Link_ID);
+		$surname = mysql_real_escape_string($_POST['surname'], $sql->Link_ID);
+		$dob_day = mysql_real_escape_string($_POST['dob_day'], $sql->Link_ID);
+		$dob_month = mysql_real_escape_string($_POST['dob_month'], $sql->Link_ID);
+		$dob_year = mysql_real_escape_string($_POST['dob_year'], $sql->Link_ID);
+		$date_of_birth = $dob_day . "/" . $dob_month . "/" . $dob_year;
+		$email = mysql_real_escape_string($_POST['email'], $sql->Link_ID);
+		$telephone_landline = mysql_real_escape_string($_POST['telephone_landline'], $sql->Link_ID);
+		$telephone_mobile = mysql_real_escape_string($_POST['telephone_mobile'], $sql->Link_ID);
+		$address_line_1 = mysql_real_escape_string($_POST['address_line_1'], $sql->Link_ID);
+		$address_line_2 = mysql_real_escape_string($_POST['address_line_2'], $sql->Link_ID);
+		$address_line_3 = mysql_real_escape_string($_POST['address_line_3'], $sql->Link_ID);
+		$postcode = mysql_real_escape_string($_POST['postcode'], $sql->Link_ID);
+		$borough_county = mysql_real_escape_string($_POST['borough_county'], $sql->Link_ID);
+		$what_secondary_school_did_you_attend = mysql_real_escape_string($_POST['what_secondary_school_did_you_attend'], $sql->Link_ID);
+		$how_did_you_hear_about_us = mysql_real_escape_string($_POST['message'], $sql->Link_ID);
+		$receive_communications = mysql_real_escape_string($_POST['receive_communications'], $sql->Link_ID);
+		$receive_communications = ($receive_communications == 'No') ? 'No' : 'Yes';
+
+		// Build INSERT query
+		$query = "INSERT INTO tbl_pre_apprenticeships_accountancy  
+		(firstname, surname, date_of_birth, email, telephone_landline, telephone_mobile, address_line_1, address_line_2, address_line_3, postcode, borough_county, what_secondary_school_did_you_attend, how_did_you_hear_about_us, receive_communications, datetime_submitted) 
+		VALUES('$firstname', '$surname', '$date_of_birth', '$email', '$telephone_landline', '$telephone_mobile', '$address_line_1', '$address_line_2', '$address_line_3', '$postcode', '$borough_county', '$what_secondary_school_did_you_attend', '$how_did_you_hear_about_us', '$receive_communications', '$date_now_mysql')";
+		$sql->query($query,$debug);
+
+		if ($sql->num_rows_affected() > 0) {
+			$added = TRUE;
+		} else {
+			$added = FALSE;
+		}
+		
+		if ($added) {
+
+			header('Location: http://www.conel.ac.uk/registration_successful');
+			exit;
+			
+		} else {
+			echo '<p>Failed to add info to database</p>';
+			header('Location: http://www.conel.ac.uk/our_courses/pre_apprenticeships_accountancy?registration=failed');
+			exit;
+		}
+		
+	} else {
+		header('Location: http://www.conel.ac.uk');
+		exit;
+	}
+?>
---
+++ layout/js/form-pre_apprenticeships_accountancy.js
@@ -0,0 +1,88 @@
+function isValidEmailAddress(emailAddress) {  
+	var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+	return pattern.test(emailAddress);  
+}
+$(document).ready(function() {
+
+	function limitChars(textid, limit, infodiv)
+	{
+		var text = $('#'+textid).val();    
+		var textlength = text.length;
+		if(textlength > limit)
+		{
+			$('#' + infodiv).html(''+limit+' character limit reached');
+			$('#'+textid).val(text.substr(0,limit));
+			return false;
+		}
+		else
+		{
+			$('#' + infodiv).html(''+ (limit - textlength) +' characters left');
+			return true;
+		}
+	}
+	 
+	$(function(){
+		 $('#preapp_acc_how_heard').keyup(function(){
+			 limitChars('preapp_acc_how_heard', 500, 'charlimitinfo');
+		 })
+	});
+	
+	$("#conel_form").submit(function(e) {
+
+		// check if first name is blank
+		var name = $("#preapp_acc_firstname").val();
+		if (name == '') {
+			alert('First Name is a required field');
+			$("#preapp_acc_firstname").focus();
+			return false;
+		}
+		
+		// check if surname is blank
+		var name = $("#preapp_acc_surname").val();
+		if (name == '') {
+			alert('Surname is a required field');
+			$("#preapp_acc_surname").focus();
+			return false;
+		}
+		
+		// check if email is blank
+		var email = $("#preapp_acc_email").val();
+		if (email == '') {
+			alert('Email is a required field');
+			$("#preapp_acc_email").focus();
+			return false;
+		}
+
+		if (!isValidEmailAddress(email)) {
+			alert('Invalid email address');
+			$("#preapp_acc_email").focus();
+			return false;
+		}
+
+		// check if second email matches the first email address
+		var email_confirm = $("#preapp_acc_email_confirm").val();
+		if (email_confirm == '') {
+			alert('Please confirm your email address');
+			$("#preapp_acc_email_confirm").focus();
+			return false;
+		}
+		
+		if (email != email_confirm) {
+			alert('Email addresses do not match');
+			return false;
+		}
+		
+		// check if how heard is blank
+		var careers_ict = $("#preapp_acc_how_heard").val();
+		if (careers_ict == '') {
+			alert('"How Did You Hear About Us?" is a required field');
+			$("#preapp_acc_how_heard").focus();
+			return false;
+		}
+		
+		
+		return true;
+
+	});
+
+});
