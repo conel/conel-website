91c573230a109cd372f86c432b60da13ef28e07e
Nathan Kowald <nkowald@conel.ac.uk>
2012-07-26 16:56:50 +0100

    delete unused files. Update unblock page, make it more secure

--- unblock.php
+++ unblock.php
@@ -2,49 +2,44 @@
 <head>
 <style type="text/css">
 body {
-font-family: Arial, Helvetica, sans-serif; 
-font-size: 10pt; 
-padding: 0px;
-margin:0px;
+    font-family: Arial, Helvetica, sans-serif; 
+    font-size: 10pt; 
+    padding: 0px;
+    margin:0px;
 }
-b{
-font-family: Arial, Helvetica, sans-serif;
-font-size:12pt;
-padding:0px;
+b {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size:12pt;
+    padding:0px;
 }
 </style>
 </head>
 <body>
 <?php
-if (empty($_REQUEST['message']) || empty($_REQUEST['usrname']))
 //if "email" is not filled out, display the form
-  {
+if (empty($_REQUEST['message']) || empty($_REQUEST['usrname'])) {
 	echo "<form method='post' action='unblock.php'><input name='URL' type='hidden' value='"; 
-	echo $_REQUEST['src']; 
+	echo htmlentities($_REQUEST['src']); 
 	echo "'><input name='cat' type='hidden' value='"; 
-	echo $_REQUEST['cat']; 
+	echo htmlentities($_REQUEST['cat']); 
 	echo "'><b>Username:</b> <input name='usrname' type='text' style='width: 207px'";
-
-	if (!empty($_REQUEST['usrname']))
-	{
-		echo "value='" . $_REQUEST['usrname'] . "'";
+	if (!empty($_REQUEST['usrname'])) {
+		echo "value='" . htmlentities($_REQUEST['usrname']) . "'";
 	}
 	echo "><br /><br /><b>Comments:</b><br /><textarea name='message' rows='7' cols='50'>";
 
-	if (!empty($_REQUEST['message']))
-	{
-		echo $_REQUEST['message'];
+	if (!empty($_REQUEST['message'])) {
+		echo htmlentities($_REQUEST['message']);
 	}
 	echo "</textarea><br /><input type='submit' /></form>";
-  }
-else
+
 //if "email" is filled out, send email
-  {
-	  //send email
-	  $message = "User: " . $_REQUEST['usrname'] . "\nURL: " . $_REQUEST['URL'] . "\n" . $_REQUEST['cat'] ."\nComments: " . $_REQUEST['message'];
-	  mail( "unblock@staff.conel.ac.uk", "Subject: Unblock web site",$message, "From: webmaster@staff.conel.ac.uk" );
-	  echo "<p><b>Your request will be reviewed and a notification sent out to you shortly.</b></p>";
-  }
+} else {
+    //send email
+    $message = "User: " . htmlentities($_REQUEST['usrname']) . "\nURL: " . htmlentities($_REQUEST['URL']) . "\n" . htmlentities($_REQUEST['cat']) ."\nComments: " . htmlentities($_REQUEST['message']);
+    mail( "unblock@staff.conel.ac.uk", "Subject: Unblock web site", $message, "From: webmaster@staff.conel.ac.uk" );
+    echo "<p><b>Your request will be reviewed and a notification sent out to you shortly.</b></p>";
+}
 ?>
 <a id="bottom"></a>
 </body>
