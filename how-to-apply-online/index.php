23aa3fc75835483092ea75a35db66a23d6c9b0f9
Nathan Kowald <nkowald@conel.ac.uk>
2013-01-15 16:48:31 +0000

    New How to Apply Online page. LinkedIn logo to footer.

--- how-to-apply-online/index.php
+++ how-to-apply-online/index.php
@@ -4,37 +4,59 @@
 <title>Online Application - How to apply online</title> 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="Distribution" content="Global" />
-<link href="css/welcome.css" rel="stylesheet" type="text/css" /> 
+<link href="css/welcome2.css" rel="stylesheet" type="text/css" /> 
 </head>
 <body>
 	<!-- holder -->
 	<div id="holder">
 		<h1><span>How to apply online</span></h1>
 		<div id="step1">
-			<h2 class="step1">Browse our courses</h2>
-                <p>Browse our courses from the home page course search. When you select course you can fill in your online application form using the <span class="apply_now">Apply <em>Now</em></span> button.</p>
-				<p>If you prefer you can download our Application Form to print, complete and return by post to us.</p>
-		</div>
-		<!--div id="step2">
-			<h2 class="step2">Send your application</h2>
-            <p>If you prefer, you can start entering your details into the online application form below and browse courses from within the form.<br />Fields marked with an asterisk are mandatory.</p>
-			<p><a href="/course-application/"><img src="images/apply-now.gif" width="218" height="57" alt="Apply Now" id="apply_now_button" /></a></p>
-		</div-->
+			<p><span style="font-size:1.1em;font-weight:bold"><img src="./images/circle1.png" style="width:35px;height:35px;vertical-align: middle;margin-right:5px" />Online</span><br />
+			   You can apply online by searching for the course you are interested in using the course search.<br />
+			   www.conel.ac.uk/our_courses/subjects<br />
+			   Click on the <span class="apply_now">Apply <em>Now</em></span> button at the end of each course description and this takes you to the online application form.
+			</p>
+			<p><span style="font-size:1.1em;font-weight:bold"><img src="./images/circle2.png" style="width:35px;height:35px;vertical-align: middle;margin-right:5px" />Paper application form</span><br />
+			   If you prefer to download and print off a paper application form, please click on the icon of an 
+			   application form below. The icon is a live link and takes you through to the form.		
+			</p>
+		</div>		
 		<div id="step2">
-			<h2 class="step2">Send your application</h2>
-                <p>Once you have sent us your online or written application form, we will carefully consider your application and get back to you shortly.</p>
-                <p>You will need to submit a reference.<br />Please print out the reference request form which is part of your application and give it to your referee. If you are unable to provide a reference please write to tell us that.</p>
+			<p><span style="font-size:1.1em;font-weight:bold"><img src="./images/circle3.png" style="width:35px;height:35px;vertical-align: middle;margin-right:5px" />You will need a reference to support your application</span><br />
+				You will need to submit a reference as part of your application. This
+				is both for online applicants and applicants applying using the paper
+				based form. To print off a reference, click on the icon below.
+				You can send in your reference by post with your application form, or
+				bring it along to your interview.
+			</p>
+			<p><span style="font-size:1.1em;font-weight:bold"><img src="./images/circle4.png" style="width:35px;height:35px;vertical-align: middle;margin-right:5px" />Returning your application form by post</span><br />
+				Please return completed applications to either:<br />
+				<span style="color:#B8C44B;">For Enfield Centre courses:</span><br />
+				<span style="color:#000;font-weight:normal;font-size:0.8em">
+					Admissions, The College of Haringey, Enfield and North East London<br />
+					Enfield Centre, 73 Hertford Road, London EN3 5HA
+				</span><br />
+				<span style="color:#B8C44B;">For Tottenham Centre courses:</span><br />
+				<span style="color:#000;font-weight:normal;font-size:0.8em">
+					Admissions, The College of Haringey, Enfield and North<br />
+					East London, Tottenham Centre, High Road,<br />
+					London N15 4RU
+				</span><br />					
+			</p>			
+			<p style="width:360px"><span style="font-size:1.1em;font-weight:bold"><img src="./images/circle5.png" style="width:35px;height:35px;vertical-align: middle;margin-right:5px" />What happens after you have applied?</span><br />
+				We will write to you by post or email within ten
+				working days of you submitting or sending in
+				your application.
+			</p>
 		</div>
 		<br class="clear_both" />
 		
-		<div id="footer">
-			<p>For courses at our Enfield Centre, please return completed applications to: Admissions: Enfield Centre, The College of Haringey, Enfield and North East London, 73 Hertford Road London EN3 5HA</p><br />
-			<p>For courses at our Tottenham Centre, please return completed applications to: Admissions: Tottenham Centre, The College of Haringey, Enfield and North East London, High Road London N15 4RU</p>
-		</div>
+		<div id="footer"></div>
 		
-		<div id="application_form"><a href="http://www.conel.ac.uk/docs/application-form-12-13_web.pdf" target="_blank"><img src="images/application-form.png" width="160px" height="220px" alt="Application Form" /></a></div>
+		<div id="application_form"><a href="http://www.conel.ac.uk/docs/application-form-12-13_web.pdf" target="_blank"><img src="images/application_form.jpg" width="160px" height="220px" alt="Application Form" /></a></div>
 		
-		<div id="reference_form"><a href="http://www.conel.ac.uk/docs/reference.pdf" target="_blank"><img src="images/application-form.png" width="160px" height="220px" alt="Reference Form" /></a></div>
+		<div id="reference_form"><a href="http://www.conel.ac.uk/docs/reference.pdf" target="_blank"><img src="images/reference_form.jpg" width="160px" height="220px" alt="Reference Form" /></a></div>
+	
 	</div>
 	<!-- //holder -->
 <script type="text/javascript"> 
--- layout/l_footerh.tpl
+++ layout/l_footerh.tpl
@@ -5,6 +5,7 @@
         <a href="http://www.facebook.com/pages/The-College-of-Haringey-Enfield-and-North-East-London/118065961539264"><img src="/layout/img/logo-facebook.jpg" alt="Like us on Facebook" width="50" height="50" /></a> 
         <a href="http://www.youtube.com/user/CONELwebmaster"><img src="/layout/img/logo-youtube.png" alt="YouTube Channel" width="48" height="48" /></a> 
         <a href="http://www.twitter.com/collegeNELondon"><img src="/layout/img/logo-twitter.jpg" alt="Follow us on Twitter" width="50" height="50" /></a> 
+        <a href="http://www.linkedin.com/company/college-of-haringey-enfield-and-north-east-london?trk=hb_tab_compy_id_2880885"><img src="/layout/img/LinkedIn_Logo60px.png" alt="LinkedIn" width="60" height="50" /></a> 
     </div>
 	<div id="fb_like">
 		<span id="footer_like"><!--<iframe src="http://www.facebook.com/plugins/like.php?app_id=127787840640162&amp;href=http%3A%2F%2Fwww.conel.ac.uk&amp;send=false&amp;layout=button_count&amp;width=110&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:110px; height:21px;" allowTransparency="true"></iframe>--></span>
